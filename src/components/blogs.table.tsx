9cae279567d9bbb822349936f9fc85a068e81fc1
phung anh van <phunganhvan13122004@gmail.com>
2026-04-16 15:52:32 +0700

    view blogs with useQuery

--- src/components/blogs.table.tsx
+++ src/components/blogs.table.tsx
@@ -5,7 +5,16 @@ import Button from 'react-bootstrap/Button';
 import BlogCreateModal from './modal/blog.create.modal';
 import BlogEditModal from './modal/blog.edit.modal';
 import BlogDeleteModal from './modal/blog.delete.modal';
+import { calculatePagesCount } from '../helper';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
+import BlogsPagination from './pagination/blogs.pagination';
 
+interface IBlog {
+    id: string;
+    title: string;
+    author: string;
+    content: string;
+}
 
 function BlogsTable() {
 
@@ -17,49 +26,36 @@ function BlogsTable() {
 
     const [isOpenDeleteModal, setIsOpenDeleteModal] = useState<boolean>(false);
     
-    const blogs = [
-        {
-            "id": 1,
-            "title": "What is Lorem Ipsum?",
-            "author": "Hỏi Dân IT",
-            "content": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        },
-        {
-            "id": 2,
-            "title": "Why do we use it?",
-            "author": "Hỏi Dân IT",
-            "content": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-        },
-        {
-            "id": 3,
-            "title": "Where does it come from?",
-            "author": "Hỏi Dân IT",
-            "content": "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,comes from a line in section 1.10.32."
-        },
-        {
-            "id": 4,
-            "title": "Why do we use it?",
-            "author": "Hỏi Dân IT",
-            "content": "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)."
-        },
-        {
-            "id": 5,
-            "title": "Where can I get some?",
-            "author": "Hỏi Dân IT",
-            "content": "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc."
-        }
-    ]
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [totalPages, setTotalPages] = useState<number>(1);
+    const PAGE_SIZE = 2;
 
-    const handleEditBlog = (blog: any) => {
+    const {isPending, error, data} = useQuery({
+        queryKey: ['fetchBlogs', currentPage],
+        queryFn: (): Promise<IBlog[]> =>
+            fetch(`http://localhost:8000/blogs?_page=${currentPage}&_limit=${PAGE_SIZE}`).then((res) =>
+            {
+                const totalItems = res.headers?.get('X-Total-Count') ?? 0;
+                if (totalItems) {
+                    setTotalPages(calculatePagesCount(PAGE_SIZE, Number(totalItems)));
+                }
+                return res.json();
+            }
+            ),
+            placeholderData: keepPreviousData,
+    })
+
+    const handleEditBlog = (blog: IBlog) => {
         setDataBlog(blog);
         setIsOpenUpdateModal(true);
     }
-
-    const handleDelete = (blog: any) => {
+    const handleDelete = (blog: IBlog) => {
         setDataBlog(blog);
         setIsOpenDeleteModal(true);
     }
+    if (isPending) return 'Loading...'
 
+    if (error) return 'An error has occurred: ' + error.message
     return (
         <>
             <div style={{ display: "flex", justifyContent: "space-between", margin: "15px 0" }}>
@@ -78,7 +74,7 @@ function BlogsTable() {
                     </tr>
                 </thead>
                 <tbody>
-                    {blogs?.map(blog => {
+                    {data?.map((blog: IBlog) => {
                         return (
                             <tr key={blog.id}>
                                 <td>{blog.id}</td>
@@ -106,7 +102,11 @@ function BlogsTable() {
                     })}
                 </tbody>
             </Table>
-
+            <BlogsPagination
+                totalPages={totalPages}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
             <BlogCreateModal
                 isOpenCreateModal={isOpenCreateModal}
                 setIsOpenCreateModal={setIsOpenCreateModal}
