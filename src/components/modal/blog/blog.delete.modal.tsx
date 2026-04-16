69879b2bbf77e5f04b0d18b20e9340319abe7e05
phung anh van <phunganhvan13122004@gmail.com>
2026-04-16 16:13:57 +0700

    feature delete blogs

--- src/components/modal/blog/blog.delete.modal.tsx
+++ src/components/modal/blog/blog.delete.modal.tsx
@@ -1,11 +1,43 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
+import { IBlog } from '../../../interface/blog.interface';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'react-toastify';
+import Spinner from 'react-bootstrap/esm/Spinner';
 
-const BlogDeleteModal = (props: any) => {
+interface IProps {
+    isOpenDeleteModal: boolean;
+    setIsOpenDeleteModal: (isOpen: boolean) => void;
+    dataBlog: IBlog;
+}
+
+const BlogDeleteModal = (props: IProps) => {
     const { dataBlog, isOpenDeleteModal, setIsOpenDeleteModal } = props;
 
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: async (payload: IBlog) => {
+            const res = await fetch(`http://localhost:8000/blogs/${payload.id}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": " application/json"
+                }
+            });
+            return res.json();
+        },
+        onSuccess: () => {
+            // data, variables, context
+            toast('🦄 Wow so easy! Delete succeed');
+            setIsOpenDeleteModal(false);
+            queryClient.invalidateQueries({ queryKey: ['fetchBlogs'] });
+        }
+    })
+
     const handleSubmit = () => {
-        console.log({ id: dataBlog?.id });
+        if (dataBlog?.id) {
+            mutation.mutate({ id: dataBlog.id, title: dataBlog.title, author: dataBlog.author, content: dataBlog.content });
+        }
     }
 
     return (
@@ -25,10 +57,26 @@ const BlogDeleteModal = (props: any) => {
                 Delete the blog: {dataBlog?.title ?? ""}
             </Modal.Body>
             <Modal.Footer>
-                <Button
-                    variant='warning'
-                    onClick={() => setIsOpenDeleteModal(false)} className='mr-2'>Cancel</Button>
-                <Button onClick={() => handleSubmit()}>Confirm</Button>
+                {!mutation.isPending ? (
+                    <>
+                        <Button
+                            variant='warning'
+                            onClick={() => setIsOpenDeleteModal(false)} className='mr-2'>Cancel</Button>
+                        <Button onClick={() => handleSubmit()}>Confirm</Button>
+                    </>
+                ) :
+                    (
+                        <Button variant="primary" disabled>
+                            <Spinner
+                                as="span"
+                                animation="border"
+                                size="sm"
+                                role="status"
+                                aria-hidden="true"
+                            />
+                            <></> Deleting...
+                        </Button>
+                    )}
             </Modal.Footer>
         </Modal>
     )
